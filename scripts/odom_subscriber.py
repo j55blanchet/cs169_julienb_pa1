3c1aed5a21efd11a839409ee381c7a117f13f584
Julien Blanchet <j55blanchet@gmail.com>
2020-01-21 11:19:39 -0500

    editing subscriber node

--- scripts/odom_subscriber.py
+++ scripts/odom_subscriber.py
@@ -1,16 +1,27 @@
+#!/usr/bin/env python
+""" odom_subscriber.py
+
+    A simple node that republishes pose messages. Not useful practically,
+    but it gives us practice with creating subscribers and publishers
+"""
 import rospy
+from std_msgs.msg import Header
+from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PoseStamped
-from visualization_msgs.msg import Marker
 
 def main():
     rospy.init_node("odom_subscriber")
+    plot_pub = rospy.Publisher("/plot_xy", PoseStamped, queue_size=1)
 
     def odom_msg_received(msg):
-        rospy.logdebug("Odom message received", msg)
+        out_msg = PoseStamped()
+        out_msg.header = Header()
+        out_msg.header.frame_id = msg.header.frame_id
+        out_msg.pose = msg.pose
+        plot_pub.publish(out_msg)
 
-    rospy.Subscriber("/odom", PoseStamped, odom_msg_received)
+    rospy.Subscriber("/pose", PoseStamped, odom_msg_received)
     rospy.spin()
-    
 
 if __name__ == "__main__":
     main()
